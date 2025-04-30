e9bd67954e6166c0f3e1d49fa5e056a53098dad2
Jaogui08 <salesfabriciojg@gmail.com>
2025-04-30 10:45:20 -0300

    vetor SENAI

---
+++ Array_nomes.java
@@ -0,0 +1,25 @@
+package Projetos_LPA.aula13;
+
+import java.util.Scanner;
+
+public class Array_nomes {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		String[] nomes = new String[5];
+		
+		for (int i=0; i<=4; i++) {
+			System.out.print("Informe o " + (i+1) + "° nome: ");
+			nomes[i] = scanner.nextLine();
+		}
+		
+		System.out.println("\nNomes recebidos:\n");
+		
+		for (int i=0; i<=4; i++) {
+			System.out.println((i+1) + "° nome: " + nomes[i]);
+		}
+		scanner.close();
+	}
+
+}
